7ce0b0377242d514660b46d5b556cdcc8334f16f
Dustin Keppler <dtkeppler@yahoo.com>
2015-09-14 19:25:47 -0400

    initial commit

---
+++ app/models/location.rb
@@ -0,0 +1,12 @@
+class Location < ActiveRecord::Base
+
+	geocoded_by :my_location
+	after_validation :geocode
+
+	#Here, behind the scenes, we will combine the address and city given to us by the User, while also adding the state (because we're only operating in Georgia here) and Geocode that!
+
+	def my_location
+		"#{address}, #{city}, GA"
+	end
+
+end
